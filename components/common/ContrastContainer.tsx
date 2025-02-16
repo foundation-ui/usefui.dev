8e75781c695a183c39ac82c6ec607d3016b44b0b
nnsncl <nuniroland@gmail.com>
2025-02-16 14:55:56 +0100

    Add ContrastContainer component

---
+++ components/common/ContrastContainer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import styled from "styled-components";
+
+export const ContrastContainer = styled.section`
+  background: var(--contrast-color);
+  border-radius: var(--measurement-medium-40);
+  padding: var(--measurement-medium-60);
+`;
