4913230d4cc839aa26c99f543ec1871feeebb6e7
nnsncl <nuniroland@gmail.com>
2025-03-29 14:45:12 +0100

    Move EmptySection component from @app to @components

--- src/app/(libraries)/_components/LibrairiesViewer.tsx
+++ src/app/(libraries)/_components/LibrairiesViewer.tsx
@@ -1,40 +1,37 @@
 "use client";
 
-import { Icon } from "@/components";
-import { Editor } from "@/features";
-import { Badge, Button, Dialog } from "@foundation-ui/components";
 import React from "react";
+import EmptySection from "@/components/shared/EmptySection";
+
+import { Editor } from "@/features";
+import { Icon } from "@/components";
+import { Dialog } from "@foundation-ui/components";
 
 function LibrairiesViewer() {
   return (
     <section className="grid w-100 h-100 align-centerr justify-center">
-      <hgroup
-        className="flex align-center justify-center"
-        style={{ flexDirection: "column", textAlign: "center" }}
-      >
-        <Badge variant="secondary" className="h-auto m-b-medium-60 ">
+      <EmptySection
+        title="No Libraries Found"
+        description="Start by creating a new library to interact with resources"
+        icon={
           <Icon className="m-y-small-60" fillOpacity={0.3}>
             <Icon.Folders />
           </Icon>
-        </Badge>
+        }
+        action={
+          <Dialog.Root>
+            <Dialog.Trigger
+              variant="primary"
+              sizing="medium"
+              style={{ color: "var(--body-color)!important" }}
+            >
+              Open Editor
+            </Dialog.Trigger>
 
-        <h1 className="fs-medium-20">No Libraries Found</h1>
-        <p className="fs-medium-10 opacity-default-30 m-b-medium-60">
-          Start by creating a new library to interact with resources.
-        </p>
-
-        <Dialog.Root>
-          <Dialog.Trigger
-            variant="primary"
-            sizing="medium"
-            style={{ color: "var(--body-color)!important" }}
-          >
-            Open Editor
-          </Dialog.Trigger>
-
-          <Editor />
-        </Dialog.Root>
-      </hgroup>
+            <Editor />
+          </Dialog.Root>
+        }
+      />
     </section>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./shared/PatternBackground";
 export * from "./shared/Icons";
 export * from "./shared/AppLink";
 export * from "./shared/Spinner";
+export * from "./shared/EmptySection";
