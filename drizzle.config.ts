f3b655ab65fb4907029aa87bd23a9ae57f47c227
nnsncl <nuniroland@gmail.com>
2025-03-31 22:22:31 +0200

    Setup db with singlestore and drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type Config } from "drizzle-kit";
 
 import { env } from "@/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    port: Number(env.SINGLESTORE_PORT),
+    database: env.SINGLESTORE_DB_NAME,
   },
-  tablesFilter: ["token-engine_*"],
 } satisfies Config;
