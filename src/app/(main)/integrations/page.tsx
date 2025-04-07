aef30ed87738de284ed991484d6f78cb591ae07e
nnsncl <nuniroland@gmail.com>
2025-04-07 21:06:29 +0200

    Add pages and loaders

---
+++ src/app/(main)/integrations/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+async function IntegrationsPage() {
+  // const libraries = await GetMock();
+
+  // if (!libraries)
+  //   return (
+  //     <hgroup className="flex align-center g-medium-30">
+  //       <h6>Fetch Error</h6>
+  //     </hgroup>
+  //   );
+
+  return <section className="w-100 h-100">WIP</section>;
+}
+
+export default IntegrationsPage;
