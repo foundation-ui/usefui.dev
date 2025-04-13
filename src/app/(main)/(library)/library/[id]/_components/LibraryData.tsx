0f1c81dcf2cf2a08da4aefbf37f976d8bb10e355
nnsncl <nuniroland@gmail.com>
2025-04-13 18:05:33 +0200

    Prepare DetailsPanel component in LibraryData

--- src/app/(main)/(library)/library/[id]/_components/LibraryData.tsx
+++ src/app/(main)/(library)/library/[id]/_components/LibraryData.tsx
@@ -2,20 +2,49 @@
 
 import React from "react";
 
+import DeleteAction from "./DeleteAction";
+
 import { MaxWidthContainer } from "@/components";
-import { Divider } from "@foundation-ui/components";
+import { TextMuted } from "@/components/shared/TextMuted";
+import { Divider, Sheet } from "@foundation-ui/components";
+import { Icon, PixelIcon } from "@foundation-ui/icons";
 
 import type { libraries_table as librariesSchema } from "@/server/db/schema";
 
 function LibraryData({ data }: { data: typeof librariesSchema.$inferSelect }) {
   return (
     <MaxWidthContainer className="g-medium-30 w-100 p-large-10">
-      <hgroup>
-        <h1 className="fs-large-10 m-b-medium-10">{data.title}</h1>
-        <p className="fs-medium-20 opacity-default-30">
-          {data.description === "" ? "No description" : data.description}
-        </p>
-      </hgroup>
+      <header className="flex justify-between align-end g-medium-30">
+        <hgroup>
+          <h1 className="fs-large-10 m-b-medium-10">{data.title}</h1>
+          <p className="fs-medium-20 opacity-default-30">
+            {data.description === "" ? "No description" : data.description}
+          </p>
+        </hgroup>
+        <Sheet.Root>
+          <div className="flex align-center g-medium-60">
+            <Sheet.Trigger variant="ghost">
+              <span className="fs-medium-10">Details</span>
+              <Icon>
+                <PixelIcon.LayoutSidebarRight />
+              </Icon>
+            </Sheet.Trigger>
+            <TextMuted className="opacity-default-10">|</TextMuted>
+            <DeleteAction libraryId={Number(data.id)} />
+          </div>
+
+          <Sheet side="right" sizing="medium">
+            <p>id:&nbsp;{data.id}</p>
+            <p>creatorId:&nbsp;{data.creatorId}</p>
+            <p>name:&nbsp;{data.name}</p>
+            <p>title:&nbsp;{data.title}</p>
+            <p>description:&nbsp;{data.description}</p>
+            <p>createdAt:&nbsp;{data.createdAt}</p>
+            <p>updatedAt:&nbsp;{data.updatedAt}</p>
+          </Sheet>
+        </Sheet.Root>
+      </header>
+
       <Divider className="m-b-medium-60" />
     </MaxWidthContainer>
   );
