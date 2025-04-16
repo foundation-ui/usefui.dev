1ef2b1b84b204ffb66e300ab4dc71f0ec2090069
nnsncl <nuniroland@gmail.com>
2025-04-16 18:56:40 +0200

    Add "wip" message in pages

--- src/app/(main)/history/page.tsx
+++ src/app/(main)/history/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import React from "react";
 
-async function HistoryPage() {
-  return <section className="w-100 h-100">WIP</section>;
+import { Badge } from "@foundation-ui/components";
+import { Icon, PixelIcon } from "@foundation-ui/icons";
+
+function HistoryPage() {
+  return (
+    <section className="w-100 h-100 p-medium-60">
+      <Badge variant="warning">
+        <Icon fill="var(--color-orange)">
+          <PixelIcon.Alert />
+        </Icon>
+        Work in Progress
+      </Badge>
+    </section>
+  );
 }
 
 export default HistoryPage;
