6f01944b267f7544a7e364a3fe1972ff295e4fb0
nnsncl <nuniroland@gmail.com>
2025-04-16 19:18:01 +0200

    Enhance library cards

--- src/app/(main)/(library)/_components/LibraryCard.tsx
+++ src/app/(main)/(library)/_components/LibraryCard.tsx
@@ -1,70 +1,47 @@
 "use client";
 
 import React from "react";
-import styled from "styled-components";
 
-import CardActions from "./CardActions";
+import Link from "next/link";
 
-import { TextMuted } from "@/components/shared/TextMuted";
-import { Divider } from "@foundation-ui/components";
-// import { Icon, PixelIcon } from "@foundation-ui/icons";
+import { Divider, Tooltip } from "@foundation-ui/components";
+import { CardBody } from "@/components";
 
+import { formatDistanceToNow } from "date-fns";
 import type { libraries_table as librariesSchema } from "@/server/db/schema";
-
-const CardBody = styled.article`
-  border: var(--measurement-small-30) solid var(--font-color-alpha-10);
-  border-radius: var(--measurement-medium-30);
-
-  box-shadow: var(--measurement-small-30) var(--measurement-small-30)
-    var(--measurement-small-10) var(--alpha-mono-darkest-10);
-
-  background: var(--body-color);
-  background-image: linear-gradient(
-    45deg,
-    var(--body-color) 0%,
-    var(--contrast-color) 100%
-  );
-
-  will-change: box-shadow;
-  transition: ease-in-out 0.2s;
-
-  &:hover {
-    box-shadow: none;
-  }
-`;
+import { Icon, PixelIcon } from "@foundation-ui/icons";
 
 function LibraryCard({
   id,
   title,
-  // published,
   description,
   updatedAt,
 }: typeof librariesSchema.$inferSelect) {
   return (
-    <CardBody className="">
-      <hgroup className="p-b-medium-30 p-t-medium-60 p-x-medium-60 grid g-medium-10 ">
-        <div className="flex align-center g-medium-10">
-          <h6 className="fs-medium-10">{title}</h6>
-          {/* <Icon
-            width={12}
-            height={12}
-            fill={published ? "var(--color-green)" : "var(--color-orange)"}
-          >
-            {published ? <PixelIcon.Cloud /> : <PixelIcon.Lock />}
-          </Icon> */}
-        </div>
+    <CardBody
+      as={Link}
+      href={`library/${String(id)}`}
+      className="p-medium-60 grid g-medium-30"
+    >
+      <hgroup>
+        <h6 className="fs-medium-20">{title}</h6>
         <p className="opacity-default-30 fs-medium-10">{description}</p>
       </hgroup>
-
       <Divider />
-      <footer className="flex justify-between align-center p-b-medium-60 p-t-medium-30 p-x-medium-60 fs-medium-10">
-        <div>
-          <span>{id}</span>
-          <TextMuted className=" opacity-default-10">|</TextMuted>
-          <span className="opacity-default-60">{updatedAt}</span>&nbsp;
-        </div>
+      <footer className="fs-medium-10 flex align-center justify-between g-medium-30">
+        <span className="opacity-default-60">
+          Updated&nbsp;
+          {formatDistanceToNow(new Date(Number(updatedAt)), {
+            addSuffix: true,
+            includeSeconds: true,
+          })}
+        </span>
 
-        <CardActions libraryId={String(id)} />
+        <Tooltip content="Go to Details">
+          <Icon>
+            <PixelIcon.Open />
+          </Icon>
+        </Tooltip>
       </footer>
     </CardBody>
   );
