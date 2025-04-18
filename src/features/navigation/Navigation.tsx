f55c4ac6c96712d9c17e12a3386a89bd526f88a2
nnsncl <nuniroland@gmail.com>
2025-04-18 22:34:14 +0200

    feat: Add current route in nav

--- src/features/navigation/Navigation.tsx
+++ src/features/navigation/Navigation.tsx
@@ -3,18 +3,30 @@
 import React from "react";
 import styled from "styled-components";
 
-import NavigationLinks from "./_components/NavigationLinks";
+import { usePathname } from "next/navigation";
 
+import NavigationLinks from "./_components/NavigationLinks";
 import { PageMenu } from "@foundation-ui/components";
 
 const NavWrapper = styled(PageMenu)`
   padding-left: 0 !important;
   border: none !important;
 `;
+const CurrentRouteLabel = styled.h6`
+  text-transform: capitalize;
+  letter-spacing: -0.7px;
+`;
 
 function Navigation() {
+  const pathname = usePathname();
+  const segments = pathname.split("/").filter(Boolean); // Split the pathname into segments and remove empty strings
+
   return (
-    <NavWrapper className="flex justify-end align-center p-r-medium-60">
+    <NavWrapper className="flex justify-between align-center p-r-medium-60">
+      <CurrentRouteLabel className="fs-medium-20">
+        {segments.at(0)}
+      </CurrentRouteLabel>
+
       <NavigationLinks />
     </NavWrapper>
   );
--- src/features/navigation/_components/NavigationLinks.tsx
+++ src/features/navigation/_components/NavigationLinks.tsx
@@ -3,21 +3,13 @@
 import React from "react";
 import SolutionsLinks from "./SolutionsLinks";
 
-import { Badge, Button } from "@foundation-ui/components";
+import { Button } from "@foundation-ui/components";
 import { Icon, PixelIcon } from "@foundation-ui/icons";
 import { TextMuted } from "@/components/shared/TextMuted";
 
 function NavigationLinks() {
   return (
     <div className="flex align-center g-medium-30">
-      <Badge variant="warning">
-        <Icon fill="var(--color-orange)">
-          <PixelIcon.Alert />
-        </Icon>
-        Work in Progress
-      </Badge>
-      <TextMuted className="fs-medium-20 opacity-default-10">|</TextMuted>
-
       <SolutionsLinks />
       <TextMuted className="fs-medium-20 opacity-default-10">|</TextMuted>
       <Button
