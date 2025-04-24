8e13243306195018b3fc46c68307ef84abc5e337
nnsncl <nuniroland@gmail.com>
2025-04-24 22:20:55 +0200

    Add clerk middleware and provider in app's layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import StyledComponentsRegistry from "@/lib/styles-registry";
 
-import { ClientProvider } from "@/providers";
+import { AuthProvider, ClientProvider } from "@/providers";
 import { Toaster } from "sonner";
 
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Foundation UI Engine",
-  description: "Design Token Engine",
+  description: "Open Source Design Tokens Engine",
 };
 
 export default function RootLayout({
@@ -16,26 +16,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <StyledComponentsRegistry>
-          <ClientProvider>
-            <div id="portal-container" />
-            <Toaster
-              toastOptions={{
-                style: {
-                  borderRadius: "var(--measurement-medium-30)",
-                  borderColor: "var(--font-color-alpha-10)",
-                  background: "var(--body-color)",
-                  color: "var(--font-color)",
-                  fontSize: "var(--fontsize-medium-10)",
-                },
-              }}
-            />
-            {children}
-          </ClientProvider>
-        </StyledComponentsRegistry>
-      </body>
-    </html>
+    <StyledComponentsRegistry>
+      <ClientProvider>
+        <AuthProvider>
+          <html lang="en">
+            <body>
+              <div id="portal-container" />
+              <Toaster
+                toastOptions={{
+                  style: {
+                    borderRadius: "var(--measurement-medium-30)",
+                    borderColor: "var(--font-color-alpha-10)",
+                    background: "var(--body-color)",
+                    color: "var(--font-color)",
+                    fontSize: "var(--fontsize-medium-10)",
+                  },
+                }}
+              />
+              {children}
+            </body>
+          </html>
+        </AuthProvider>
+      </ClientProvider>
+    </StyledComponentsRegistry>
   );
 }
