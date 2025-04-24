fdf2c39456404c25980a4d83cc89ac7b83598cab
nnsncl <nuniroland@gmail.com>
2025-04-24 22:19:06 +0200

    Update UserAvatar, SignIn/Out actions, Profile header  according to auth state

--- src/features/sidebar/_components/ProfileActions.tsx
+++ src/features/sidebar/_components/ProfileActions.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import styled from "styled-components";
 
 import { useColorMode } from "@foundation-ui/tokens";
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
 
+import ProfileAvatar from "./ProfileAvatar";
+import ProfileHeader from "./ProfileHeader";
+
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
 import {
-  Avatar,
   Divider,
   DropdownMenu,
   Tooltip,
@@ -22,12 +24,6 @@ const SilentItem = styled(DropdownMenu.Item)`
     cursor: default;
   }
 `;
-const AvatarXSmall = styled(Avatar)`
-  background: var(--font-color-alpha-10) !important;
-
-  max-width: var(--measurement-medium-60) !important;
-  max-height: var(--measurement-medium-60) !important;
-`;
 
 function ProfileActions() {
   const { colorMode, setColorMode } = useColorMode();
@@ -36,25 +32,11 @@ function ProfileActions() {
     <DropdownMenu.Root>
       <DropdownMenu>
         <DropdownMenu.Trigger variant="ghost" rawicon>
-          <AvatarXSmall sizing="small">
-            <Icon viewBox="0 0 24 24" height={32} width={32} fillOpacity={0.3}>
-              <WebIcon.Incognito />
-            </Icon>
-          </AvatarXSmall>
+          <ProfileAvatar />
         </DropdownMenu.Trigger>
 
         <DropdownMenu.Content>
-          <SignedOut>
-            <hgroup className="p-medium-30">
-              <p className="fs-medium-20 opacity-default-60">
-                You&apos;re Incognito
-              </p>
-              <p className="fs-medium-10 opacity-default-30">
-                Login to get started&nbsp;<b>for free</b>
-              </p>
-            </hgroup>
-          </SignedOut>
-
+          <ProfileHeader />
           <Divider />
           <SilentItem
             radio
@@ -139,7 +121,7 @@ function ProfileActions() {
             <SignOutButton>
               <DropdownMenu.Item className="flex align-center g-medium-30 justify-between fs-medium-10">
                 Sign Out
-                <Icon viewBox="0 0 18 18">
+                <Icon>
                   <PixelIcon.Logout />
                 </Icon>
               </DropdownMenu.Item>
