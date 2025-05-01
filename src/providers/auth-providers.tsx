9927dd334a56436527c3726fa96aa01cce76e658
nnsncl <nuniroland@gmail.com>
2025-05-01 21:36:47 +0200

    Add styles variables to clerk provider

--- src/providers/auth-providers.tsx
+++ src/providers/auth-providers.tsx
@@ -1,19 +1,81 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-base-to-string */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
 
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { useColorMode } from "@foundation-ui/tokens";
+
+import {
+  GetColorTokenBase,
+  GetTokenFromSource,
+  js_design_tokens,
+} from "@foundation-ui/tokens";
 
 export function AuthProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { colorMode } = useColorMode();
+
+  const colors = () => {
+    if (colorMode === "dark") {
+      return {
+        body: GetColorTokenBase({
+          source: js_design_tokens,
+          token_category: "color",
+          query: "mono-darker",
+        }),
+        font: GetColorTokenBase({
+          source: js_design_tokens,
+          token_category: "color",
+          query: "mono-white",
+        }),
+      };
+    }
+
+    return {
+      body: GetColorTokenBase({
+        source: js_design_tokens,
+        token_category: "color",
+        query: "mono-white",
+      }),
+      font: GetColorTokenBase({
+        source: js_design_tokens,
+        token_category: "color",
+        query: "mono-darkest",
+      }),
+    };
+  };
+
+  const borderRadius = `${
+    GetTokenFromSource({
+      source: js_design_tokens,
+      token_category: "measurement",
+      query: "medium",
+      unit: "px",
+    })?.base
+  }px`;
+
   return (
     <ClerkProvider
       afterSignOutUrl={"/sign-up"}
       appearance={{
         layout: {
           animations: false,
+        },
+        variables: {
+          borderRadius,
+          colorBackground: colors().body,
+          colorInputBackground: colors().body,
+          colorPrimary: `${colors().font}60`,
+          colorTextSecondary: colors().font,
+          colorTextOnPrimaryBackground: colors().body,
+          colorInputText: colors().font,
+          colorNeutral: colors().font,
+          colorText: colors().font,
         },
       }}
     >
