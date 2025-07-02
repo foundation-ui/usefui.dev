147ca0ff37c41447ac147ed626270fbadba0c99b
nnsncl <nuniroland@gmail.com>
2025-07-02 22:01:33 +0200

    Add field component docs

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -18,6 +18,7 @@ import CollapsibleTemplate from "../_components/_templates/CollapsibleTemplate";
 import DialogTemplate from "../_components/_templates/DialogTemplate";
 import DividerTemplate from "../_components/_templates/DividerTemplate";
 import DropdownMenuTemplate from "../_components/_templates/DropdownMenuTemplate";
+import FieldTemplate from "../_components/_templates/FieldTemplate";
 
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { getAllDocSlugs, getDocBySlug } from "@/lib/docs";
@@ -66,6 +67,7 @@ export default async function DocPage({ params }: DocPageProps) {
     DialogTemplate,
     DividerTemplate,
     DropdownMenuTemplate,
+    FieldTemplate,
   };
 
   return (
