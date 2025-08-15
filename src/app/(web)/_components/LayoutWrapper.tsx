29d6d1fcb3966f9ec6338f2437d5548f0df0789d
nnsncl <nuniroland@gmail.com>
2025-08-15 15:14:42 +0200

    Extract LayoutWrapper to prevent build errors

---
+++ src/app/(web)/_components/LayoutWrapper.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "styled-components";
+import { Page } from "@usefui/components";
+
+export const LayoutWrapper = styled(Page.Content)`
+  background-image: url(/vignette.webp);
+  background-size: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center bottom;
+
+  background-blend-mode: overlay;
+  mix-blend-mode: overlay;
+`;
