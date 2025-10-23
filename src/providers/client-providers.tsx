3fb9df7933d6c8af0afb1834cdc8c5b5f3272e1b
nnsncl <nuniroland@gmail.com>
2025-10-23 20:30:03 +0200

    Enhance ds color tokens

--- src/providers/client-providers.tsx
+++ src/providers/client-providers.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ColorModeProvider, js_design_tokens } from "@usefui/tokens";
 
 import {
+  generateAlpha,
   generateColorClasses,
   generateCSSVariables,
   generateDimensionClasses,
@@ -28,9 +29,54 @@ const cssOpacityClasses = generateOpacityClasses(design_tokens.opacity);
 const cssColorClasses = generateColorClasses(
   js_design_tokens.design_tokens.color,
 );
+
 const cssVariables = generateCSSVariables({
   name: js_design_tokens.name,
-  design_tokens,
+  design_tokens: {
+    color: [
+      ...design_tokens.color,
+
+      {
+        name: "mono-dark",
+        base: { hex: "#1d1b15" },
+        alpha: generateAlpha("#1d1b15"),
+        tint: [],
+        shade: [],
+      },
+      {
+        name: "mono-darker",
+        base: { hex: "#1b1913" },
+        alpha: generateAlpha("#1b1913"),
+        tint: [],
+        shade: [],
+      },
+      {
+        name: "mono-darkest",
+        base: { hex: "#14120b" },
+        alpha: generateAlpha("#14120b"),
+        tint: [],
+        shade: [],
+      },
+      {
+        name: "mono-white",
+        base: { hex: "#ebeae5" },
+        alpha: generateAlpha("#ebeae5"),
+        tint: [],
+        shade: [],
+      },
+      {
+        name: "mono-light",
+        base: { hex: "#f0efeb" },
+        alpha: generateAlpha("#f0efeb"),
+        tint: [],
+        shade: [],
+      },
+    ],
+    measurement: [...design_tokens.measurement],
+    fontsize: [...design_tokens.fontsize],
+    opacity: [...design_tokens.opacity],
+    depth: [...design_tokens.depth],
+  },
 });
 
 const CSSRoot = createGlobalStyle`
@@ -59,6 +105,8 @@ const CSSRoot = createGlobalStyle`
 		--breakpoint-desktop-small: 1240px;
 		--breakpoint-desktop: 1440px;
 		--breakpoint-desktop-large: 1589px;
+
+
 	}
 `;
 
@@ -71,11 +119,11 @@ export function ClientProvider({
   const colorModeConfig = React.useMemo(
     () => ({
       body: {
-        light: "var(--color-mono-light)",
+        light: "var(--color-mono-white)",
         dark: "var(--color-mono-darkest)",
       },
       contrast: {
-        light: "var(--color-mono-white)",
+        light: "var(--color-mono-light)",
         dark: "var(--color-mono-darker)",
       },
     }),
