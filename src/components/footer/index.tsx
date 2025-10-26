0eca96564b128387e307dcbb9fbc9623a18bf62b
nnsncl <nuniroland@gmail.com>
2025-10-26 22:02:25 +0100

    Add color-mode in footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,10 +3,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const FooterWrapper = styled.footer`
-  border-top: var(--measurement-small-30) solid var(--font-color-alpha-10);
-  flex-wrap: wrap;
+import { ColorModes } from "../";
+import { Badge } from "@usefui/components";
 
+const FooterWrapper = styled.footer`
+  /* border-top: var(--measurement-small-30) solid var(--font-color-alpha-10); */
+  flex-wrap: wrap;
+  width: 100%;
+  margin: 0 auto;
   a {
     text-decoration: none;
   }
@@ -14,7 +18,7 @@ const FooterWrapper = styled.footer`
 
 function Footer() {
   return (
-    <FooterWrapper className="p-x-medium-60 p-y-large-10 flex justify-center align-center g-medium-10">
+    <FooterWrapper className="p-x-medium-60 p-y-large-10 flex justify-between align-center g-medium-60">
       <p className="fs-medium-10">
         <span className="opacity-default-60">Built by</span>&nbsp;
         <a target="_blank" href="https://github.com/nnsncl">
@@ -24,18 +28,11 @@ function Footer() {
         <a target="_blank" href="https://github.com/foundation-ui">
           Foundation UI
         </a>
-        .
-        <span className="opacity-default-60">
-          &nbsp;The source code is available on&nbsp;
-        </span>
-        <a
-          target="_blank"
-          href="https://github.com/foundation-ui/design-system"
-        >
-          Github
-        </a>
-        .
       </p>
+
+      <Badge variant="border">
+        <ColorModes />
+      </Badge>
     </FooterWrapper>
   );
 }
