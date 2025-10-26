e879c1bf1e20ec1c5a87e8ed66d7487b5f4268b4
nnsncl <nuniroland@gmail.com>
2025-10-26 22:01:34 +0100

    Update color-mode grid

--- src/components/color-mode/index.tsx
+++ src/components/color-mode/index.tsx
@@ -9,7 +9,7 @@ function ColorModes() {
   const { setColorMode } = useColorMode();
 
   return (
-    <div className="flex align-center g-small-60">
+    <div className="flex align-center g-medium-30">
       <Tooltip content="Light">
         <Button variant="ghost" onClick={() => setColorMode("light")}>
           <Icon>
@@ -17,7 +17,6 @@ function ColorModes() {
           </Icon>
         </Button>
       </Tooltip>
-      <span className="opacity-default-10 fs-medium-10 p-x-medium-10">|</span>
       <Tooltip content="Dark">
         <Button variant="ghost" onClick={() => setColorMode("dark")}>
           <Icon>
@@ -25,7 +24,6 @@ function ColorModes() {
           </Icon>
         </Button>
       </Tooltip>
-      <span className="opacity-default-10 fs-medium-10 p-x-medium-10">|</span>
       <Tooltip content="System">
         <Button variant="ghost" onClick={() => setColorMode("system")}>
           <Icon>
