ea854ab8a2c696b4bcc44004f11ca6a2d33527e7
nnsncl <nuniroland@gmail.com>
2025-10-31 19:33:21 +0100

    Add responsive behavior to DocSidebar

--- src/app/docs/_components/DocsSidebar.tsx
+++ src/app/docs/_components/DocsSidebar.tsx
@@ -1,14 +1,43 @@
 "use client";
 
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import DocsSidebarLinks from "./DocsSidebarLinks";
 
-import { Avatar, Page, Toolbar, Tooltip } from "@usefui/components";
-import { Icon, PixelIcon, SocialIcon } from "@usefui/icons";
+import {
+  Page,
+  Portal,
+  ScrollArea,
+  Sheet,
+  Toolbar,
+  Tooltip,
+  useToolbar,
+} from "@usefui/components";
+import { Icon, PixelIcon } from "@usefui/icons";
 
-const SidebarWrapped = styled(Toolbar)`
+const Hide = css`
+  display: none;
+  width: 0;
+  height: 0;
+  user-select: none;
+`;
+const MobileWrapper = styled.div`
+  @media (min-width: 768px) {
+    ${Hide}
+  }
+`;
+const DesktopWrapper = styled.div`
+  @media (max-width: 768px) {
+    ${Hide}
+  }
+`;
+const SidebarWrapper = styled(Toolbar)`
+  position: relative;
+  border: none !important;
+  min-width: 0 !important;
+  padding: 0 !important;
+
   @media (max-width: 768px) {
     display: none !important;
     height: 0 !important;
@@ -16,56 +45,96 @@ const SidebarWrapped = styled(Toolbar)`
   }
 `;
 
-function DocsSidebar() {
-  const hotkey = "k";
-  const binkey = "ctrlKey";
+const HOTKEY = "b";
+const BINDKEY = "ctrlKey";
+
+function TriggerIcon() {
+  return (
+    <span className="flex align-center p-y-small-60">
+      <Icon>
+        <PixelIcon.LayoutSidebarLeft />
+      </Icon>
+    </span>
+  );
+}
+function Triggers() {
+  const { methods } = useToolbar();
+  const { toggleToolbar } = methods;
+
+  const handleOpenMobileNav = () => {
+    if (toggleToolbar) toggleToolbar(true);
+  };
 
   return (
-    <Toolbar.Root>
-      <SidebarWrapped
-        side="left"
-        sizing="large"
-        height="auto"
-        shortcut
-        bindkey={binkey}
-        hotkey={hotkey}
-        defaultOpen
-      >
-        <Toolbar.Section
-          className="flex align-start justify-start m-b-medium-70"
-          showoncollapse
-        >
-          <Toolbar.Item
-            showfirstchild
-            className="flex align-center justify-between w-100"
-          >
-            <Avatar
-              style={{
-                background: "var(--font-color)",
-                borderRadius: "var(--measurement-medium-30)",
-              }}
-            >
-              <Icon fill="var(--body-color)">
-                <SocialIcon.Foundation />
-              </Icon>
-            </Avatar>
+    <Portal container="nav-portal">
+      <MobileWrapper>
+        <Tooltip content="Open Menu">
+          <Sheet.Trigger onClick={handleOpenMobileNav}>
+            <TriggerIcon />
+          </Sheet.Trigger>
+        </Tooltip>
+      </MobileWrapper>
+      <DesktopWrapper>
+        <Tooltip content={`Sidebar Ctrl+${HOTKEY.toUpperCase()}`}>
+          <Toolbar.Trigger>
+            <TriggerIcon />
+          </Toolbar.Trigger>
+        </Tooltip>
+      </DesktopWrapper>
+    </Portal>
+  );
+}
 
-            <Tooltip content={`Ctrl + ${hotkey}`}>
-              <Toolbar.Trigger variant="ghost" sizing="small">
-                <Icon>
-                  <PixelIcon.LayoutSidebarLeft />
-                </Icon>
-              </Toolbar.Trigger>
-            </Tooltip>
-          </Toolbar.Item>
-        </Toolbar.Section>
-
-        <Page.Wrapper $navigations={1.9} $menus={0}>
-          <Toolbar.Section className="h-100">
+function DesktopSidebar() {
+  return (
+    <SidebarWrapper
+      side="left"
+      sizing="large"
+      height="auto"
+      shortcut
+      hotkey={HOTKEY}
+      bindkey={BINDKEY}
+      defaultOpen
+    >
+      <Page.Wrapper $navigations={0} $menus={0.9}>
+        <Toolbar.Section className="h-100 w-100">
+          <ScrollArea className="w-100 h-100 p-l-medium-30" scrollbar>
             <DocsSidebarLinks />
-          </Toolbar.Section>
+          </ScrollArea>
+        </Toolbar.Section>
+      </Page.Wrapper>
+    </SidebarWrapper>
+  );
+}
+function MobileSidebar() {
+  return (
+    <MobileWrapper>
+      <Sheet side="left" sizing="small">
+        <div className="flex justify-end m-b-medium-30">
+          <Sheet.Trigger>
+            <Icon>
+              <PixelIcon.Close />
+            </Icon>
+          </Sheet.Trigger>
+        </div>
+        <Page.Wrapper $navigations={0} $menus={0.9}>
+          <ScrollArea className="w-100 h-100">
+            <DocsSidebarLinks />
+          </ScrollArea>
         </Page.Wrapper>
-      </SidebarWrapped>
+      </Sheet>
+    </MobileWrapper>
+  );
+}
+
+function DocsSidebar() {
+  return (
+    <Toolbar.Root>
+      <Sheet.Root>
+        <Triggers />
+        <DesktopSidebar />
+        <MobileSidebar />
+      </Sheet.Root>
     </Toolbar.Root>
   );
 }
